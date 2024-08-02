80a2b6dae550eec15834a75ecc34fb88943e00b0
Victor2275 <gusev0219@gmail.com>
2024-08-02 11:46:02 -0700

    Day 1 and Day 2

---
+++ Day_1/HelloWorld.py
@@ -0,0 +1,24 @@
+import math
+
+#Excerises Level 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+print("Victor")
+print("Gusev")
+print("USA")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+
+
+#Excerises Level 3
+print(math.sqrt((10-2)**2 + (8-3)**2))
---
+++ Day_2/Variables.py
@@ -0,0 +1,47 @@
+#Day 2: 30 Days of python programming
+
+#Excerises Level 1
+firstName = "Victor"
+lastName = "Gusev"
+fullName = firstName + " " + lastName
+country = "USA"
+city = "Walnut Creek"
+age = 17
+year = 2007
+isMarried = False
+is_true = True
+is_light_on = True
+
+hasSiblings, hasPets = True, False
+
+print(type(firstName))
+print(type(lastName))
+print(type(fullName))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(isMarried))
+print(type(is_true))
+print(type(is_light_on))
+print(type(hasSiblings))
+print(type(hasPets))
+print(len(firstName))
+print(len(lastName))
+print(len(firstName) > len(lastName))
+
+#Excerises Level 2
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = int(input())
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 3.14 * 2 * radius
