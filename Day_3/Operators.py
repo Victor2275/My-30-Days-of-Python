ac08b6d04d524f3a8226453d675b55d380b0ea58
Victor2275 <gusev0219@gmail.com>
2024-08-03 22:23:37 -0700

    Day 3

---
+++ Day_3/Operators.py
@@ -0,0 +1,62 @@
+print()
+print()
+#4.
+#base = float(input("Enter base: "))
+print("Enter base: 3")
+base = 3
+#height = float(input("Enter height: "))
+print("Enter height: 6")
+height = 6
+area = 0.5 * base * height
+print("The area is", area)
+print()
+
+#5. 
+#a = float(input("Enter side a: "))
+print("Enter side a: 3")
+a = 3
+#b = float(input("Enter side b: "))
+print("Enter side b: 2")
+b = 2
+#c = float(input("Enter side c: "))
+print("Enter side c: 9")
+c = 9
+perimeter = a + b + c
+print("The perimeter of the triangle is ", perimeter)
+print()
+
+#11.
+#x = float(input("Enter x: "))
+print("Enter x: -3")
+x = -3
+y = x**2 + 6*x + 9
+
+print(y)
+print()
+
+#12.
+print(len("Dragon") < len("Python"))
+print()
+
+#13.
+print("on" in "Dragon" and "on" in "Python")
+print()
+
+#14
+print("jargon" in "I hope this course is not full of jargon")
+print()
+
+#18
+print(7//3 == int(2.7))
+print()
+
+#20
+print(int(9.8) == 10)
+print()
+
+#22
+#years = int(input("Enter number of years you have lived: "))
+print("Enter number of years you have lived: ")
+years = 3
+print("You have lived for", years * 365 * 24 * 60 * 60, "years")
+print()
